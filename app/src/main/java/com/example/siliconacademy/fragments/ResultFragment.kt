a5a260e20e8e0999a9273be5979fe74965681a63
dostonbek2000 <husanovdostonbek1010@gmail.com>
2025-03-07 07:07:02 +0500

    new

--- app/src/main/java/com/example/siliconacademy/fragments/ResultFragment.kt
+++ app/src/main/java/com/example/siliconacademy/fragments/ResultFragment.kt
@@ -10,11 +10,18 @@ import androidx.navigation.fragment.findNavController
 import com.example.siliconacademy.R
 import com.example.siliconacademy.databinding.FragmentResultBinding
 
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 class ResultFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
 private lateinit var binding: FragmentResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
 
     override fun onCreateView(
@@ -28,7 +35,16 @@ binding.add.setOnClickListener {
 
         return binding.root
     }
-
+  companion object{
+      fun newInstance(param1:String,param2:String){
+          ResultFragment().apply {
+              arguments=Bundle().apply {
+                  putString(ARG_PARAM1,param1)
+                  putString(ARG_PARAM2,param2)
+              }
+          }
+      }
+  }
 
 
 }
